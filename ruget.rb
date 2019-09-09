95afab7a462ec101a74dcf24dba3cd6e6782f3b9
Keisuke Toyota <hm.pudding0715@gmail.com>
2019-09-10 00:52:14 +0900

    commit

--- ruget.rb
+++ ruget.rb
@@ -2,7 +2,7 @@ require "formula"
 
 HOMEBREW_RUGET_VERSION="0.1.0"
 
-class Toyotter < Formula
+class Ruget < Formula
   url "https://github.com/KeisukeToyota/ruget/releases/download/v#{HOMEBREW_RUGET_VERSION}/ruget-v#{HOMEBREW_RUGET_VERSION}-x86_64-apple-darwin.zip"
   homepage "https://github.com/KeisukeToyota/ruget"
   sha256 "088b065570c67f938296e2172d21ce5012b9093fa07f4b1685247a6236f59e66"
