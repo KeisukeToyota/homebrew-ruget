7bec96bb74aabe32838fc2c494060e8f292940da
KeisukeToyota <hm.pudding0715@gmail.com>
2019-12-25 08:49:45 +0900

    commit

--- ruget.rb
+++ ruget.rb
@@ -3,12 +3,12 @@ require "formula"
 HOMEBREW_RUGET_VERSION="0.1.2"
 
 class Ruget < Formula
-  url "https://github.com/KeisukeToyota/ruget/releases/download/v#{HOMEBREW_RUGET_VERSION}/ruget-v#{HOMEBREW_RUGET_VERSION}-x86_64-apple-darwin.zip"
-  homepage "https://github.com/KeisukeToyota/ruget"
+  url "https://github.com/ksk001100/ruget/releases/download/v#{HOMEBREW_RUGET_VERSION}/ruget-v#{HOMEBREW_RUGET_VERSION}-x86_64-apple-darwin.zip"
+  homepage "https://github.com/ksk001100/ruget"
   sha256 "bfdad338dab2e8d3f26b6e3a74b71b9d07eb53ad9231c12abe226d0e65bf5bef"
 
   version "v#{HOMEBREW_RUGET_VERSION}"
-  head "https://github.com/KeisukeToyota/ruget.git", :branch => "master"
+  head "https://github.com/ksk001100/ruget.git", :branch => "master"
 
   def install
     bin.install "ruget"
