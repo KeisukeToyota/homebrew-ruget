9d8990a24ca299db134a4ec4f590696ac3f4c01d
ksk001100 <hm.pudding0715@gmail.com>
2020-01-21 07:52:12 +0900

    v0.2.0

--- ruget.rb
+++ ruget.rb
@@ -1,11 +1,11 @@
 require "formula"
 
-HOMEBREW_RUGET_VERSION="0.1.2"
+HOMEBREW_RUGET_VERSION="0.2.0"
 
 class Ruget < Formula
   url "https://github.com/ksk001100/ruget/releases/download/v#{HOMEBREW_RUGET_VERSION}/ruget-v#{HOMEBREW_RUGET_VERSION}-x86_64-apple-darwin.zip"
   homepage "https://github.com/ksk001100/ruget"
-  sha256 "bfdad338dab2e8d3f26b6e3a74b71b9d07eb53ad9231c12abe226d0e65bf5bef"
+  sha256 "071b2890a9aba15572cb7e831f8b183520d596f8eca90e7919856aeb95c4a8a0"
 
   version "v#{HOMEBREW_RUGET_VERSION}"
   head "https://github.com/ksk001100/ruget.git", :branch => "master"
